fa234e8e6a8d68ffdc7455ff3309e0fe3e297f0f
lihaozhe <hz.li@mayibida.com>
2024-07-08 10:28:00 +0800

    feat(live template): add apache version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "live-templates-plus-apache"
+rootProject.name = "live-templates-plus-apache-commons"
